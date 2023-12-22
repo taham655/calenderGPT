665fbfe4cc6af390d8e6eb5a0ab211d89fb173c4
taham655 <96670924+taham655@users.noreply.github.com>
2023-12-22 22:36:17 +0800

    adding files

---
+++ calenderService.py
@@ -0,0 +1,64 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from dateutil.parser import parse
+from datetime import timedelta
+import datetime
+import os 
+
+class GoogleCalendar:
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    def __init__(self):
+        self.service = self.get_calendar_service()
+
+    def get_calendar_service(self):
+        creds = None
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        return build('calendar', 'v3', credentials=creds)
+
+    def add_event(self, start_time_str, summary, location=None, end_time_str=None, description=None, is_recurring=False, recurrence_rule=None):
+        start_time = parse(start_time_str)
+
+        if not end_time_str:
+            end_time = start_time + timedelta(minutes=15)
+            end_time_str = end_time.isoformat()
+
+        event = {
+            'summary': summary,
+            'location': location,
+            'description': description,
+            'start': {
+                'dateTime': start_time_str,
+                'timeZone': 'Asia/Kuala_Lumpur',
+            },
+            'end': {
+                'dateTime': end_time_str,
+                'timeZone': 'Asia/Kuala_Lumpur',
+            }
+        }
+
+        if is_recurring and recurrence_rule:
+            event['recurrence'] = [recurrence_rule]
+
+        created_event = self.service.events().insert(calendarId='primary', body=event).execute()
+        print('Event created: %s' % (created_event.get('htmlLink')))
+
+# Usage
+calendar = GoogleCalendar()
+calendar.add_event('2023-12-23T12:00:00', summary='Quick Meeting', location='Office')
+
+# For a recurring event
+# recurrence_rule = 'RRULE:FREQ=WEEKLY;BYDAY=MO'
+# calendar.add_event('2023-12-21T8:00:00', '2023-12-21T13:00:00', 'Wake up ', 'Office', 'Team weekly sync-up', is_recurring=True, recurrence_rule=recurrence_rule)
--- main.py
+++ main.py
@@ -1,55 +1,28 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
-import datetime
-import os 
+from dotenv import load_dotenv
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
 
-# If modifying these SCOPES, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+load_dotenv()
 
 
-def get_calendar_service():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+llm = OpenAI()
+# chat_model = ChatOpenAI()
+# memory = ConversationBufferWindowMemory( k=1)
 
-    service = build('calendar', 'v3', credentials=creds)
-    return service
 
-def add_event_to_calendar(start_time_str, end_time_str, summary, location, description):
-    service = get_calendar_service()
+conversation_with_summary = ConversationChain(
+    llm=llm,
+    # We set a low k=2, to only keep the last 2 interactions in memory
+    memory=ConversationBufferWindowMemory(k=2),
+    verbose=True
+)
 
-    event = {
-        'summary': summary,
-        'location': location,
-        'description': description,
-        'start': {
-            'dateTime': start_time_str,
-            'timeZone': 'Asia/Kuala_Lumpur',
-        },
-        'end': {
-            'dateTime': end_time_str,
-            'timeZone': 'Asia/Kuala_Lumpur',
-        },
-    }
-
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created: %s' % (event.get('htmlLink')))
-
-# Call the function
-add_event_to_calendar('2023-12-20T13:00:00', '2023-12-20T14:00:00', 'having a cup of tea with jack', '3R036', 'talking about football')
+while True:
+    user_input = input(">>> ")
+    if user_input == "exit":
+        break
+    else:
+        response = conversation_with_summary.predict(input=user_input)
+        print(response)
